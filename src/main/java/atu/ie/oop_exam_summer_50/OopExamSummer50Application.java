0698c60f23077b8615fb148674bc47438f2568fe
EmilijusOnusaitis-ST <g00465644@atu.ie>
2026-03-02 12:14:15 +0000

    finish

--- src/main/java/atu/ie/oop_exam_summer_50/OopExamSummer50Application.java
+++ src/main/java/atu/ie/oop_exam_summer_50/OopExamSummer50Application.java
@@ -1,13 +1,72 @@
 package atu.ie.oop_exam_summer_50;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
-public class OopExamSummer50Application {
+public class OopExamSummer50Application<Input> {
 
     public static void main(String[] args) {
         SpringApplication.run(OopExamSummer50Application.class, args);
     }
 
+    public class reservationId{
+@PutMapping("validate/{id}")
+        Input validate(
+                @PathVariable @Positive Long id,
+                @RequestParam ("query") @NotEmpty String query,
+                @Valid @RequestBody Input input){
+    return input;
 }
+
+    }
+@ComponentScan
+
+    public class studentemail{
+@PutMapping("validate/{email}")
+Input validate(
+        @PathVariable @Positive Long email,
+        @RequestParam ("query") @NotEmpty String query,
+        @Valid @RequestBody Input input){
+    return input;
+}
+}
+
+
+    }
+    @Configuration
+    public class equipmentTag{
+//what equipment selection
+    }
+
+    @Configuration
+    public class reservationDate{
+// calender month and date to select
+    }
+
+@Service
+    public class startHour{
+//what hour to start input
+
+    }
+
+@Service
+    public class durattionHours{
+
+//input
+//@min(1) @max(12) Hours
+
+    }
+}
+
--- src/test/java/atu/ie/oop_exam_summer_50/OopExamSummer50ApplicationTests.java
+++ src/test/java/atu/ie/oop_exam_summer_50/OopExamSummer50ApplicationTests.java
@@ -1,7 +1,13 @@
 package atu.ie.oop_exam_summer_50;
 
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 
 @SpringBootTest
 class OopExamSummer50ApplicationTests {
@@ -9,5 +15,21 @@ class OopExamSummer50ApplicationTests {
     @Test
     void contextLoads() {
     }
+@Email
+void studentEmail{
+        String message() Default {"input student email"};
+        Class<studentEmail> groups() Default{};
+
+
+}
+@Size
+
+class input{
+        @Min(1) @Max(12)
+    int durationHours;
+        return durattionHours
+
+
+}
 
 }
