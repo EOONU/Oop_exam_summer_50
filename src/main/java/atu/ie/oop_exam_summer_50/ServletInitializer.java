6c0d7cbfbb87cc4ea4f10b6e2415c5eef0fd9699
EmilijusOnusaitis-ST <g00465644@atu.ie>
2026-03-02 11:07:55 +0000

    intial commit

---
+++ src/main/java/atu/ie/oop_exam_summer_50/ServletInitializer.java
@@ -0,0 +1,13 @@
+package atu.ie.oop_exam_summer_50;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OopExamSummer50Application.class);
+    }
+
+}
